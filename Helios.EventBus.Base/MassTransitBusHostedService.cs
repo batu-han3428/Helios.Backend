2c9ba66b7fe082982cefcab9936e3d01e3826e16
Batuhan FINDIK <batuhanf@med-case.io>
2023-08-22 17:05:19 +0300

    -- path düzeltildi

---
+++ Helios.EventBus.Base/MassTransitBusHostedService.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using MassTransit.Util;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helios.EventBus.Base
+{
+    public class MassTransitBusHostedService : IHostedService
+    {
+        private readonly IBusControl _busControl;
+
+        public MassTransitBusHostedService(IBusControl busControl)
+        {
+            _busControl = busControl;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                TaskUtil.Await(() => _busControl.StartAsync(), cancellationToken);
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _busControl.StopAsync(cancellationToken);
+        }
+    }
+}
