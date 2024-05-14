7f8241e21a5b396a095fd4a8371e559f64d9772f
Somayeh JANGI <somayehj@med-case.io>
2024-05-14 10:38:57 +0300

    All packages updated.

--- Helios.EDC/Helios.Common/Model/ModuleModel.cs
+++ Helios.EDC/Helios.Common/Model/ModuleModel.cs
@@ -7,6 +7,10 @@ namespace Helios.Common.Model
         public Int64 Id { get; set; }
         public Int64 TenantId { get; set; }
         public Int64 UserId { get; set; }
+        public Int64 AddedById { get; set; }
+        public Int64 UpdatedById { get; set; }
+        public string AddedNameAndLastName { get; set; }
+        public string UpdatedNameAndLastName { get; set; }
         public string Name { get; set; }
         [Column(TypeName = "datetime")]
         public DateTimeOffset CreatedAt { get; set; }
