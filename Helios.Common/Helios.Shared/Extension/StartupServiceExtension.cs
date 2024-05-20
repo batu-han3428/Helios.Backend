af1b816a68ac46441067073b8acbc6a984e4acaa
Somayeh JANGI <somayehj@med-case.io>
2024-05-20 09:17:16 +0300

    - New shared project added - Common project loacated in another place.

---
+++ Helios.Common/Helios.Shared/Extension/StartupServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿using Helios.eCRF.Models;
+using StackExchange.Redis;
+
+namespace Helios.Shared.Extension
+{
+    public static class StartupServiceExtension
+    {
+        public static IServiceCollection SharedDefaultConfigurationService(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddHttpContextAccessor();
+            services.AddSingleton<IConnectionMultiplexer>(provider =>
+            {
+                var redisOptions = Configuration.GetSection("Redis").Get<RedisOptions>();
+                var connectionString = $"{redisOptions.Host}:{redisOptions.Port},password={redisOptions.Password},abortConnect=false";
+                return ConnectionMultiplexer.Connect(connectionString);
+            });
+
+            services.AddSignalR();
+            return services;
+        }
+    }
+}
