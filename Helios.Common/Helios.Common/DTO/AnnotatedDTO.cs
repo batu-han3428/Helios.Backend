b7b5d7370be8b9b1b3126759cc13a54a5344eb7d
Batuhan FINDIK <batuhanf@med-case.io>
2024-07-03 09:48:03 +0300

    common projesinde classlar silinmiş. tekrar ekledim.

---
+++ Helios.Common/Helios.Common/DTO/AnnotatedDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Helios.Common.DTO
+{
+    public class AnnotatedDTO
+    {
+        public bool IsPage { get; set; }
+        public bool IsCalculated { get; set; }
+        public bool IsLabel { get; set; }
+        public bool IsDesc { get; set; }
+        public bool IsAdditional { get; set; }
+        public string? IsVersion { get; set; }
+    }
+}
