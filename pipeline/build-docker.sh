75118812f9972530f408b0fc9088da161e93b490
İnan Şen <joulesta87@hotmail.com>
2024-08-14 21:44:45 +0200

    d

--- pipeline/build-docker.sh
+++ pipeline/build-docker.sh
@@ -4,10 +4,10 @@ TAG=$3
 DOCKERFILE_PATH=$4
 
 # Build the Docker image
-docker build -t $IMAGE_NAME:$TAG -f $DOCKERFILE_PATH .
+sudo docker build -t $IMAGE_NAME:$TAG -f $DOCKERFILE_PATH .
 
 # Tag the image with the registry
-docker tag $IMAGE_NAME:$TAG $REGISTRY/$IMAGE_NAME:$TAG
+sudo docker tag $IMAGE_NAME:$TAG $REGISTRY/$IMAGE_NAME:$TAG
 
 # Push the image to the Azure Container Registry
-docker push $REGISTRY/$IMAGE_NAME:$TAG
+sudo docker push $REGISTRY/$IMAGE_NAME:$TAG
