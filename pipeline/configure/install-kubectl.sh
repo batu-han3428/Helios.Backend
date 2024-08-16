ab18dd5aeeb91d7ac61063b174cb579b156151c1
İnan Şen <joulesta87@hotmail.com>
2024-08-17 00:25:10 +0200

    d

---
+++ pipeline/configure/install-kubectl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install dependencies
+apt-get update && apt-get install -y apt-transport-https curl
+
+# Download and install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+# Verify installation
+kubectl version --client
