3317d916f2f56d4a8853cfca457d1cd48753adba
İnan Şen <joulesta87@hotmail.com>
2024-08-18 02:29:33 +0200

    dd

--- Helios.EDC/Helios.Core/Program.cs
+++ Helios.EDC/Helios.Core/Program.cs
@@ -39,16 +39,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        // Swagger UI'ýn kök dizinine yönlendirme
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    // Swagger UI'ýn kök dizinine yönlendirme
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseCors("MyPolicy");
 
